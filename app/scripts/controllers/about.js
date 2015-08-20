9311902e14d4f407f9549ca00807b241ddc2bcc9
Glenn <nausleyg@gmail.com>
2015-08-20 16:13:40 -0500

    Intial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wiffleBallManagerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the wiffleBallManagerApp
+ */
+angular.module('wiffleBallManagerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wiffleBallManagerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wiffleBallManagerApp
+ */
+angular.module('wiffleBallManagerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
