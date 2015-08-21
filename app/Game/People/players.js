9ba3a603b98e939ca62d03381ef9697c272e436f
Glenn <nausleyg@gmail.com>
2015-08-20 21:37:15 -0500

    Added the pitching method to the players

--- app/Game/People/players.js
+++ app/Game/People/players.js
@@ -13,8 +13,7 @@ var randNum = function(max, min){
         max = 10;
     }
     return Math.floor(Math.random()*max)+min;
-}
-
+};
 
 /**
  * The contructer for the individual player objects
@@ -46,6 +45,7 @@ function Player(params){
         power: false,
         contact: false
     }
+    this.pitchCount = 0;
 
     //Stats
     //pastSeasons and playoffs is formatted like JSON, where each season is a key to the stats object
@@ -64,13 +64,13 @@ function Player(params){
  * @param  {object} pitch the pitch object from the pitcher
  * @return {object}       swing object
  */
-.Player.prototype.swing = function (self, pitch) {
+Player.prototype.swing = function (self, pitch) {
     //an array of the parts of a pitch (objects), that inlfuence the batter
-    var parts = Object.keys(pitch).filter(function(part){ return ['vert', 'lat', 'strike', 'curve'].indexOf(part) !== -1 }).map(function(part){
+    var parts = Object.keys(pitch).filter(function(part){ return ['vert', 'hori', 'strike', 'curve'].indexOf(part) !== -1 }).map(function(part){
         return {
             part: part,
             type: pitch[part],
-            seen: randNum(100) < self.attributes.eye * 4;
+            seen: randNum(100) < self.attributes.eye * 4
         }
     });
     var swing = {};
@@ -88,6 +88,10 @@ function Player(params){
         }
     })
 
+    pitch.count.current = parts.filter(function(part){ return part.part === 'strike' && part.seen})[0];
+    if(pitch.count.current){
+        pitch.count.current = pitch.count.current.type;
+    }
     //the pitch.count needs to be an array of strings with [balls, strikes, current pitch strike?]
     swing.type = self.useBatBrain(self, pitch.count.reduce(function(agg, curr){ return agg + curr }));
     return swing;
@@ -135,4 +139,125 @@ Player.prototype.useBatBrain = function (self, key) {
         };
         return 'contact';
     }
+};
+
+/**
+ * Access the brain to determine what to pitch
+ * @param  {object} self       player object passed through as this
+ * @param  {object} indicators the ingame batter indiactors that each player has
+ * @return {object}            Object with all the details of the pitch; to be used by the batter for swing
+ */
+Player.prototype.pitch = function (self, indicators) {
+    var key = Object.keys(indicators).reduce(function(agg, curr){ return agg + indicators[curr]; }), pitch = {};
+    var concept = self.pitchBrain[key];
+    if(concept){
+        Object.keys(concept).forEach(function(part){
+            pitch[part] = Object.keys(concept[part]).reduce(function(old, curr){
+                if(concept[old] && conept[old].SLG > concept[curr].SLG){
+                    return curr;
+                }else{
+                    return old
+                }
+            });
+        });
+    }else {
+        self.pitchBrain[key] = {
+            vert: {
+                high: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                low: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                middle: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                }
+            },
+            hori: {
+                inside: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                outside: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                middle: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                }
+            },
+            strike: {
+                yes: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                no: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+            },
+            curve: {
+                yes: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+                no: {
+                    attempts: 1,
+                    hits: 1,
+                    bases: 1,
+                    BA: 0,
+                    SLG: 0,
+                },
+            }
+        }
+        return self.pitch(self, indicators);
+    }
+
+    //changing whether the pitch is a strike or a ball based on a stamina check
+    if(randNum(100) < self.pitchCount - (self.attributes.pitchStamina + self.attributes.pitchControl)*4){
+        if(pitch.strike === 'yes'){
+            pitch.strike = 'no';
+        } else{
+            pitch.strike = 'yes';
+        }
+    }
+
+    pitch.count = {
+        balls: indicators.balls,
+        strikes: indicators.strikes
+    }
+
+    self.pitchCount = self.pitchCount + 1;
+    return pitch;
 };
